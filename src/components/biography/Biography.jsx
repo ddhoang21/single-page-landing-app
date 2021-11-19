d1f5763e2fc429db6e74a594e888724eec271749
DDH <ddhoang1993@gmail.com>
2021-11-19 00:06:28 -0800

    team section added

--- src/components/biography/Biography.jsx
+++ src/components/biography/Biography.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './biography.css';
 
-const Biography = () => {
+const Biography = ({ image, name, status, text }) => {
     return (
-        <div>
-            
+        <div className='team-container_biography'>
+            {/* <div className='team-container_biography-image'>
+                <img scr={image} alt='team-member' />
+            </div> */}
+            <div className='team-container_biography-content'>
+                <div>
+                    <h3 className='gradient__text'>{name}</h3>
+                    <h4>{status}</h4>
+                </div>
+                <p>{text}</p>          
+            </div>
         </div>
+
     )
 }
 
-export default Biography
+export default Biography;
--- src/containers/team/Team.jsx
+++ src/containers/team/Team.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Biography from '../../components/biography/Biography';
+// import Z  from '../../assets/vkbe-Z.png';
+// import DH from '../../assets/vkbe-DH.png';
 import './team.css';
 
 const Team = () => {
@@ -6,6 +9,10 @@ const Team = () => {
         <div className='team section__padding' id='team'>
             <div className='team-heading'>
                 <h1 className='gradient__text'>Meet our team</h1>
+            </div>
+            <div className='team-container'>
+                <Biography name='Zlatko Porobic' status='Co-Founder' text='Zlatko excels at designing and developing high performing systems. He builds enterprise grade applications with a strong expertise in real-time stream processing. Along with his deep technical background, he emphasizes the importance of mature build and release pipelines to ensure efficient software development life cycles. He is driven by the concept of infrastructure as code.'/>
+                <Biography name='Duc-Huy Do' status='Co-Founder' text='Duc-Huy has experience in delivering end-to-end products from various industries. He works closely with stake-holders from engineers, leadership to end-users for minimizing any risk during the development phase. He focuses on designing the cloud infrastructure and the application development with experience in serverless architecture for web-based applications. He is a cyber-security enthusiast.'/>
             </div>
         </div>
     )
