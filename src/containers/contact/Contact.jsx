1eee284f3e329c6296014655cb5758ecb3c6a65a
DDH <ddhoang1993@gmail.com>
2021-11-19 00:20:56 -0800

    added contact section

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -3,8 +3,16 @@ import './contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className='contact' id='contact'>
+            <div className='contact-content'>
+                <p>Interested in our services ?</p>
+                <h3>Schedule a free consultation today.</h3>
+            </div>
+            <div className='contact-btn'>
+                <a href='https://calendly.com/vkbe' target='_blank' rel='noreferrer'>
+                    <button type='button'>Get Started</button>
+                </a>
+            </div>
         </div>
     )
 }
